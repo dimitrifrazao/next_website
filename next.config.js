42bdec8384aca1dcdea45b0a5895a468a731d32f
dimitri <dimitrifrazao@gmail.com>
2024-08-19 12:22:25 -0400

    tetsing removing basePath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: process.env.NODE_ENV === "production" ? "/next_website" : "",
+  basePath: "",
   output: "export",
   images: {
     unoptimized: true,
